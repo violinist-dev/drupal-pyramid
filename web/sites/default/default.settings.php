fb4e56f76628b3df4969ff479c3d620a47618dce
MatthieuScarset <m@matthieuscarset.com>
2017-10-21 16:39:11 -0400

    Add salt.txt generation and use it in settings file

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -295,7 +295,8 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = file_get_contents($app_root . '/' . $site_path . '/salt.txt');
+
 
 /**
  * Deployment identifier.
